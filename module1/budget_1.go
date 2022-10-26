cc34814d47c754bd4bc9876e2670ae60d6249a96
Ivan Munteanu <ivan@munteanu.co>
2022-10-26 10:01:08 -0400

    done

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -1,5 +1,13 @@
 package module1
 
 // Budget stores budget information
+type Budget struct {
+	Items []Item
+	Max   float32
+}
 
 // Item stores item information
+type Item struct {
+	Description string
+	Price       float32
+}
